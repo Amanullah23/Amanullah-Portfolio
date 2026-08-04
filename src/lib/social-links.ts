151f8143f3b2b516be2068b43457a49dedb7d826
Amanullah23 <amanyawari220@gmail.com>
2026-08-04 15:05:13 +0430

    social link updated

--- src/lib/social-links.ts
+++ src/lib/social-links.ts
@@ -2,10 +2,10 @@ export const socialLinks = {
   github: "https://github.com/Amanullah23",
   linkedin: "#", // TODO: replace with your real LinkedIn URL
   whatsapp: "https://wa.me/93787484323",
-  upwork: "https://www.upwork.com/freelancers/~mock", // TODO: replace
-  fiverr: "https://www.fiverr.com/mock", // TODO: replace
-  facebook: "https://facebook.com/mock", // TODO: replace
-  instagram: "https://instagram.com/mock", // TODO: replace
-  threads: "https://threads.net/@mock", // TODO: replace
-  x: "https://x.com/mock", // TODO: replace
+  upwork: "https://www.upwork.com/freelancers/amanullah_yawari", // TODO: replace
+  fiverr: "https://www.fiverr.com/s/38zozyx", // TODO: replace
+  facebook: "https://www.facebook.com/share/198oMzJwrG/", // TODO: replace
+  instagram: "https://www.instagram.com/amanullah_yawari?igsh=YjF4ZHJjZ29la2Rt", // TODO: replace
+  threads: "https://www.threads.com/@amanullah_yawari", // TODO: replace
+  x: "https://x.com/theyawari", // TODO: replace
 };
